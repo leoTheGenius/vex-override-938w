6d9d57633fd4d40a70c36cefe7d1ce8992e2b86b
Leo Wu <leowu1425@gmail.com>
2026-07-20 08:52:41 -0700

    arcade

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include "main.h"
 
 pros::Controller master(pros::E_CONTROLLER_MASTER);
-pros::Motor front_left(1, pros::v5::MotorGears::green, pros::v5::MotorUnits::degrees);
-pros::Motor front_right(10, pros::v5::MotorGears::green, pros::v5::MotorUnits::degrees);
-pros::Motor back_left(2, pros::v5::MotorGears::green, pros::v5::MotorUnits::degrees);
-pros::Motor back_right(9, pros::v5::MotorGears::green, pros::v5::MotorUnits::degrees);
+pros::Motor frontleft(1, pros::v5::MotorGears::green, pros::v5::MotorUnits::degrees);
+pros::Motor frontright(10, pros::v5::MotorGears::green, pros::v5::MotorUnits::degrees);
+pros::Motor backleft(2, pros::v5::MotorGears::green, pros::v5::MotorUnits::degrees);
+pros::Motor backright(9, pros::v5::MotorGears::green, pros::v5::MotorUnits::degrees);
 
 void initialize() {
-	front_right.set_reversed(true);
-	back_right.set_reversed(true);
-	pros::screen::print(TEXT_MEDIUM, 1, "Arcade Drive Ready");
+	frontright.set_reversed(true);
+	backright.set_reversed(true);
 }
 
 void disabled() {}
@@ -23,13 +22,13 @@ void opcontrol() {
 		int forward = master.get_analog(ANALOG_LEFT_Y);
 		int turn = master.get_analog(ANALOG_RIGHT_X);
 
-		int left_power = forward - turn;
-		int right_power = forward + turn;
+		int leftp = forward - turn;
+		int rightp = forward + turn;
 
-		front_left.move(left_power);
-		back_left.move(left_power);
-		front_right.move(right_power);
-		back_right.move(right_power);
+		frontleft.move(leftp);
+		backleft.move(leftp);
+		frontright.move(rightp);
+		backright.move(rightp);
 
 		pros::delay(20);
 	}
